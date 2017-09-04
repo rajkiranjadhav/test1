37dae21ca70a1e993960eb682724aa8d8a1ef1c7
rajkiran <rajkiran.jadhav89@gmail.com>
2017-09-04 11:28:54 +0530

    config change

--- TestGit/src/com/rule/Test.java
+++ TestGit/src/com/rule/Test.java
@@ -2,19 +2,25 @@ package com.rule;
 
 
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.log4j.xml.DOMConfigurator;
 
+import com.util.MyLogger;
 import com.util.VtransactLogger;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		 DOMConfigurator.configure("conf/log4j.xml");
-         VtransactLogger.writeLog("Initalizing App launcher...", 0);
+		MyLogger.initializeLogger();
 		
+		MyLogger.writeLog("method invoked", 1);
 		System.out.println("Hi4");
+		MyLogger.writeLog("method end", 1);
 		
 	}
 
---
+++ TestGit/src/com/util/MyLogger.java
@@ -0,0 +1,79 @@
+package com.util;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class MyLogger {
+	
+	
+	
+	
+	private MyLogger()
+	{
+		
+	}
+	
+	
+	
+	public static void main(String []args) throws IOException
+	{
+		
+		initializeLogger();
+	}
+	
+	
+	public static void initializeLogger() throws IOException
+	{
+		
+		
+		FileReader fileReader=new FileReader("conf/config.properties");
+		
+		Properties properties=new Properties();
+		properties.load(fileReader);
+		
+		 DOMConfigurator.configure(properties.getProperty("log.FilePath"));
+		 MyLogger.writeLog("=========initializeLogger========", 0);
+		 
+		 
+		
+		
+		
+	}
+	
+	
+	
+	public static void writeLog(String msg,long id) throws IOException
+	{
+		
+		StackTraceElement[] stackTraceElements=Thread.currentThread().getStackTrace();
+		
+		
+		String className=stackTraceElements[2].getClassName();
+		String methodName=stackTraceElements[2].getMethodName();
+		int lineNumber=stackTraceElements[2].getLineNumber();
+		
+		Logger logger= Logger.getLogger(className);
+		logger.info("|"+className+"|"+methodName+"|"+lineNumber+"|"+msg+"|FC-"+id);
+		
+		
+	}
+	
+
+	public static void writeLogDebug(String msg,long id)
+	{
+		
+	}
+	
+	
+	public static void writeLogError(String msg,long id)
+	{
+		
+	}
+	
+}
