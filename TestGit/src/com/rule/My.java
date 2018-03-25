ad132a150a26a5853ac74abf2de1dd4d2cdf859c
rajkiran2 <jrajkiran1@yahoo.co.in>
2018-03-25 20:09:24 +0530

    my class changed

--- TestGit/src/com/rule/My.java
+++ TestGit/src/com/rule/My.java
@@ -1,5 +1,26 @@
 package com.rule;
 
 public class My {
+	
+	int id;
+	String name;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 
 }
